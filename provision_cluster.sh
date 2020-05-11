9a7b27f7a29fd815011aff5ad178c13a5293291d
Robert Love <terp4life2001@gmail.com>
2020-05-10 18:16:43 -0700

    enable both workers

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ansible-playbook -i inventory.yaml playbooks/worker.yaml
+sudo ANSIBLE_ROLES_PATH=/home/rwlove/ansible/roles ansible-playbook -i inventory.yaml playbooks/worker.yaml
