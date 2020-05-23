4e7b922908de4578c711ee0e1668aff20ceeeec8
Robert Love <terp4life2001@gmail.com>
2020-05-22 18:01:12 -0700

    split master from worker

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+sudo ANSIBLE_ROLES_PATH=/home/rwlove/ansible/roles ansible-playbook -i inventory.yaml playbooks/master.yaml
 sudo ANSIBLE_ROLES_PATH=/home/rwlove/ansible/roles ansible-playbook -i inventory.yaml playbooks/worker.yaml
