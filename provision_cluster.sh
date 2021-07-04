db8c4335eaa621d1efa9fd1a71e5cf4e1a7179a7
Robert Love <terp4life2001@gmail.com>
2021-07-04 10:22:04 -0400

    update-provisioning

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo ANSIBLE_ROLES_PATH=/home/rwlove/ansible/roles ansible-playbook -i inventory.yaml playbooks/master.yaml
-#sudo ANSIBLE_ROLES_PATH=/home/rwlove/ansible/roles ansible-playbook -i inventory.yaml playbooks/worker.yaml
+sudo ANSIBLE_ROLES_PATH=/home/rwlove/ansible/roles ansible-playbook -i inventory.yaml playbooks/worker.yaml
